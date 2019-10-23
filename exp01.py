4fe0494536a4e4aae1382668b254ce53a92df7de
vh-diazr <56886734+vh-diazr@users.noreply.github.com>
2019-10-22 19:03:22 -0700

    Update exp01.py
    Code debugging and line comments

--- exp01.py
+++ exp01.py
@@ -1,73 +1,50 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Oct 18 09:08:06 2016
+Created on Tue Oct 22, 2019
 
-@author: ASUS
+@author: VHDR
 """
-
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Sep  9 12:11:17 2016
-
-@author: ASUS
-"""
-
-# -*- coding: utf-8 -*-
 """
 Spyder Editor
 
 This is a temporary script file.
 """
 
+# Import required libraries
 from scipy import *
-from pylab import *
-from scipy.misc import imresize
-from scipy.misc import imsave
-import cv2
-from cv2.ximgproc import guidedFilter
-from skimage.restoration import denoise_tv_chambolle, denoise_bilateral
+import numpy as np
+import matplotlib.pyplot as plt
 
-def KNB(mw,K):
-    r,c = mw.shape
-    cent = mw[int(floor(r/2)), int(floor(c/2))]
-    dist = zeros([r,c])
-    dist = abs(mw - cent)
-    dist_ord = sort(ravel(dist[:]))
-    dist_K = dist_ord[K-1]
-    x,y = where(dist <= dist_K)
-    nbh = mw[x,y]
-    nbh = ravel(sort(nbh))
-    nbh = nbh[0:K]
-    return nbh
-
-def EV(mw,ev):
-    r,c = mw.shape
-    cent = mw[int(floor(r/2)), int(floor(c/2))]
-    x,y = where( mw >= (cent-ev) )
-    nbh0 = ravel(mw[x,y])
-    x = where( nbh0 <= (cent + ev) )
-    nbh = nbh0[x]
-    return nbh
 
 def rgb2gray(img):
     img_gray = double( (0.2989*img[:,:,0] + 0.5870*img[:,:,0] + 0.1140*img[:,:,0])) # RGB to Gray Conversion
     return img_gray
         
-### Construction of a synthetic image degraded with haze
+# Read image from disk
 im = 255*double( imread("peppers.png") )
+# Obtain image size
 Nr,Nc = im.shape
 
-Nsample = 9
+Nsample = 100 #Number of noisy samples
 
-ens = zeros([Nr,Nc,Nsample])
-sigN = 25.0
+ens = np.zeros([Nr,Nc,Nsample]) #Contained for nosy image stochestic process
+
+sigN = 25.0 #Additive noise variance
+
+#Create a nosy image ensamble
 for k in range(Nsample):
     noise = sigN * np.random.randn(Nr,Nc)
     ens[:,:,k] = im + noise
-    figure(1), imshow(abs(ens[:,:,k])), gray()
+    plt.figure(1)
+    plt.title('Stochastic process of the noisy image')
+    plt.imshow(abs(ens[:,:,k]), cmap='gray')
     pause(0.001)
+    plt.show()
+    plt.clf()
     
-out = zeros([Nr,Nc])
+out = zeros([Nr,Nc]) #Container of the processed image
+
+#Estimation (Maximum-likelihood estimator) of the output image
 for k in range(Nr):
     for l in range(Nc):
         out[k,l] = mean(ens[k,l,:])
@@ -76,11 +53,11 @@ inp  = ens[:,:,0]
 print('Done!')
 
                         
-MAE = mean( abs( im - out )**2 )
+MAE = mean( abs( im - out )**2 ) #Calculation of the Mean-Absolute-Error
 
 leyend = 'Mean Absolute Error: ' + str(MAE)
 print(leyend)
 #y2 = y2/y2[:].max()
-subplot(131), imshow(uint8(im)), title('Undegraded Image'), gray()
-subplot(132), imshow(uint8(abs(inp))), title('Noisy Image')
-subplot(133), imshow(uint8(abs(out))), title('Processed Image')
+subplot(131), plt.imshow(uint8(im), cmap='gray'), plt.title('Undegraded Image')
+subplot(132), plt.imshow(uint8(abs(inp)), cmap='gray'), plt.title('Noisy Image')
+subplot(133), plt.imshow(uint8(abs(out)), cmap='gray'), plt.title('Processed Image')
